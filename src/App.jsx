d8784c8143c0e111c4006d6ccea0a5a9131da58a
21f3002339 <21f3002339@ds.study.iitm.ac.in>
2026-05-10 00:15:30 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import img1 from './assets/1.jpeg'
-import img2 from './assets/2.jpg'
+import img2 from './assets/2.JPG'
 import img3 from './assets/3.jpg'
 import img4 from './assets/4.jpg'
 import img5 from './assets/5.jpeg'
